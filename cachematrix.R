2174059bbd239803a247a8df07c2c011149075fa
Yogesh More <alexthemed@hotmail.com>
2016-07-07 13:25:55 -0400

    revised cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The following function takes a matrix, and returns
+## a list, containing the matrix as the first/"matrix" element,
+## and a spot for its inverse in the "inverse" label
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse=NULL
+  list(matrix=x, inverse=inverse)
 }
 
 
@@ -12,4 +15,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$matrix
+  inv<-x$inverse
+  if (!is.null(inv)) {
+    return(inv)
+  }
+  inv<-solve(m)
+  ##x$inverse<-inv
+  inv
 }
